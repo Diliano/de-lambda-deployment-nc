809b4f4d8688de713744b51092a1cb00d9d69b37
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-23 11:07:48 +0100

    create the run-checks job in the workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "<name of your s3 bucket here>"
-    key = "de-s3-file-reader/terraform.tfstate"
+    bucket = "cicd-bucket-nc-2024"
+    key    = "de-s3-file-reader/terraform.tfstate"
     region = "eu-west-2"
   }
 }
@@ -16,12 +16,12 @@ provider "aws" {
   region = "eu-west-2"
   default_tags {
     tags = {
-      ProjectName = "S3 File Reader Demo"
-      Team = "Data Engineering"
-      DeployedFrom = "Terraform"
-      Repository = "de-lambda-deployment"
-      CostCentre = "DE"
-      Environment = "dev"
+      ProjectName   = "S3 File Reader Demo"
+      Team          = "Data Engineering"
+      DeployedFrom  = "Terraform"
+      Repository    = "de-lambda-deployment"
+      CostCentre    = "DE"
+      Environment   = "dev"
       RetentionDate = "2024-05-31"
     }
   }
